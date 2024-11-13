7a954e21168c2660334e09b9079b735b2ef2fbc3
myeonggil <echosoul1994@gmail.com>
2024-11-13 20:30:57 +0900

    [UPDATE] rds sg and subnet group

--- infra/rds/variables.tf
+++ infra/rds/variables.tf
@@ -1,6 +1,6 @@
 variable "service_name" {}
 variable "region" {}
-variable "tiny_subnet_ids" {}
+variable "tiny_pub_subnet_ids" {}
 variable "tiny_sg_ids" {}
 variable "availability_zones" {
   description = "find availability_zones from region map"
